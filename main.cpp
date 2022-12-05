7457636ee91c6c09b52024552cd99d4599842f79
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-05 14:37:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+bool giovannino(char &a);
 
 int main(){
+    char a;
+    cin >> a;
+    if(giovannino(a))
+        cout<<a;
+    else cout <<"errore";
   return 0;
 }
