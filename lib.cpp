fa9f33fda2caad3ddd5e0a46184d566559982a9a
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-05 14:38:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created by antho on 05/12/2022.
+//
 
+#include "lib.h"
+bool giovannino(char &a){
+    if(a>=97&&a<=122){
+        a=a-32;
+        return true;
+    }
+    else if(a>=65&&a<=90){
+        a=a+32;
+        return true;
+    }
+    else return false;
+}
